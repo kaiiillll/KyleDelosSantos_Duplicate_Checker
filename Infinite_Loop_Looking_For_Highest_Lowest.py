fec350380d30d4f99878d873e7f05bfcc2bfd1de
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-03-14 18:20:01 +0800

    Infinite_Loop_Looking_For_Highest_Lowest

--- Infinite_Loop_Looking_For_Highest_Lowest.py
+++ Infinite_Loop_Looking_For_Highest_Lowest.py
@@ -1,4 +1,20 @@
+# ask the user to input numbers that is valid
+print("Please Enter numbers: ")
+
+numbers = []  
+count = 0
+
+while True:  # Loop continuing until the input is invalid
+    user_input = input("Enter a number: ")
+    count += 0
+    
+    try:
+        number = int(user_input)  
+        numbers.append(number)  
+        count += 1  
+    except:
+        print("That is Invalid!!!!") 
+        break # stopping when the input is invali
 
 sorted_numbers = sorted(numbers, reverse=True)  
-
 print("Numbers sorted from highest to lowest:", sorted_numbers)
