5a0cda11be3ec1358a0ad7e56147bcbf6681dc9f
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-03-14 17:02:41 +0800

    Average_of_given_Numbers

---
+++ Infinite_Loop_Looking_For_Average.py
@@ -0,0 +1,7 @@
+# the operation of if-else statement for getting the average
+if count > 0:  
+    average = sum(numbers) / count  
+    print("The average is:", average )  
+else:  
+    print("No valid numbers were entered.")  
+
