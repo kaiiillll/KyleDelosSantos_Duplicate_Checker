adec925e146b498d10d7d16e2e4eef746e58ae35
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-03-14 16:11:39 +0800

    max_if_else_statement

--- Infinite_Loop_Looking_Highest_Number.py
+++ Infinite_Loop_Looking_Highest_Number.py
@@ -1,23 +1,8 @@
-# ask the user to input ten numbers
-print("Please Enter numbers: ")
-
-numbers = []  
-duplicates = set()  
-seen = set()  
-
-while True:  # Loop continuing until the input is invalid
-    user_input = input("Enter a number: ")
-
-    if user_input.isdigit():  
-        number = int(user_input)
-        numbers += [number]  
     
-    if numbers:  
-        highest = max(numbers)  
+if numbers:  
+    highest = max(numbers)  
     print("Highest number entered:", highest)  
-    else:  
-        print("No valid numbers were entered.")  
+else:  
+    print("No valid numbers were entered.")  
 
-else:
-    print("That is Invalid!!!!") 
-        break # stopping when the input is invalid
+
