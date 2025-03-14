702a5aa4724af497939bcaa611c2bf80b0e3e5e7
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-03-14 17:35:31 +0800

    Infinite_Loop_Looking_For_Average

--- Infinite_Loop_Looking_For_Average.py
+++ Infinite_Loop_Looking_For_Average.py
@@ -1,7 +1,26 @@
+# ask the user to input numbers that is valid
+print("Please Enter numbers: ")
+
+numbers = []  
+count = 0
+
+while True:  # Loop continuing until the input is invalid
+    user_input = input("Enter a number: ")
+    count += 0
+    
+    try:
+        number = int(user_input)  
+        numbers.append(number)  
+        count += 1  
+    except:
+        print("That is Invalid!!!!") 
+        break # stopping when the input is invali
+
 # the operation of if-else statement for getting the average
 if count > 0:  
     average = sum(numbers) / count  
     print("The average is:", average )  
 else:  
     print("No valid numbers were entered.")  
+
 
