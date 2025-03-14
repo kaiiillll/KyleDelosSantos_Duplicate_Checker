f49e107965a4ab41c1acbd9840bb215f17deb600
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-03-14 16:20:04 +0800

    Infinite_Loop_Looking_Highest_Number

--- Infinite_Loop_Looking_Highest_Number.py
+++ Infinite_Loop_Looking_Highest_Number.py
@@ -1,4 +1,18 @@
-    
+# ask the user to input numbers that is valid
+print("Please Enter numbers: ")
+
+numbers = []  
+
+while True:  # Loop continuing until the input is invalid
+    user_input = input("Enter a number: ")
+
+    if user_input.isdigit():  
+        number = int(user_input)
+        numbers += [number]  
+    else:
+        print("That is Invalid!!!!") 
+        break # stopping when the input is invalid    
+
 if numbers:  
     highest = max(numbers)  
     print("Highest number entered:", highest)  
