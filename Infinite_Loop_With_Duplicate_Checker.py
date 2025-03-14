a2be10ce9590e75aa2ff626979119c8db5719d5d
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-03-14 15:00:55 +0800

    Infinite_Loop_With_Duplicate_Checker

--- Infinite_Loop_With_Duplicate_Checker.py
+++ Infinite_Loop_With_Duplicate_Checker.py
@@ -1,12 +1,28 @@
+# ask the user to input ten numbers
+print("Please Enter numbers: ")
+
+numbers = []  
+duplicates = set()  
+seen = set()  
 
 while True:  # Loop continuing until the input is invalid
     user_input = input("Enter a number: ")
 
     if user_input.isdigit():  
-        numbers.append(int(user_input))  
+        number = int(user_input)
+        numbers.append(number) 
+    
+        if number  in seen: # use if else statement 
+            duplicates.add(number)  
+        else:
+            seen.add(number)  
     else:
-        print("That is Invalid!!!!") # stopping when the input is invalid
-        break
+        print("That is Invalid!!!!") 
+        break # stopping when the input is invalid
 
+print("You entered:", numbers)
 
-
+if duplicates: # print the numbers with duplication
+    print("Duplicate numbers:", duplicates)
+else:
+    print("There is no Duplicates")
