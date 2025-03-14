dc61d431be7bb4fd5842e43cf01d4c0956937d05
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-03-14 14:43:46 +0800

    while true loop

---
+++ Infinite_Loop_With_Duplicate_Checker.py
@@ -0,0 +1,15 @@
+
+while True:  # Loop continuing until the input is invalid
+    user_input = input("Enter a number: ")
+
+    if user_input.isdigit():  
+        numbers.append(int(user_input))  
+    else:
+        print("That is Invalid!!!!") # stopping when the input is invalid
+        break  
+    
+
+
+
+
+
